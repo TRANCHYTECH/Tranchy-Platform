d3af534817d75497af918880abb637323acadcdd
Tau Dang <dangngoctau@gmail.com>
2023-08-12 00:17:09 +0700

    Updated env configs

--- src/Frontend/tranchy/apps/agencyportal/src/environments/environment.prod.ts
+++ src/Frontend/tranchy/apps/agencyportal/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    askApiBaseUrl: "https://ca-vg-tn-askapi-dev-sa-001.lemonrock-c20817fb.southeastasia.azurecontainerapps.io"
+    askApiBaseUrl: "https://askapi.vietgeeks.io"
   }
