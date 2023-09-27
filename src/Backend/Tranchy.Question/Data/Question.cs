edcf53abe41888e2737e601748e99957664cfb94
Tau Dang Ngoc <tau.dangngoc@medgate.ch>
2023-09-27 18:03:24 +0700

    init question structure

--- src/Backend/Tranchy.Question/Data/Question.cs
+++ src/Backend/Tranchy.Question/Data/Question.cs
@@ -10,7 +10,16 @@ namespace Tranchy.Question.Data
     [Collection("Question")]
     public class Question : Entity, ICreatedOn
     {
-        public string Title { get; set; } = default!;
+        public required string Title { get; set; }
+
         public DateTime CreatedOn { get; set; } = default!;
+    }
+
+    [Collection("QuestionEvent")]
+    public class QuestionEvent : Entity, ICreatedOn
+    {
+        public required string Event { get; set; } // Message, VideoCall, AudioCall, ScheduleCall, Like
+
+        public required DateTime CreatedOn { get; set; }
     }
 }
