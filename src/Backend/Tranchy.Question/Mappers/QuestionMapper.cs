9d4e8f9a210d0cabd89054a1eeb70ffd3f9f9d8d
TauDang <dangngoctau@gmail.com>
2023-11-01 20:17:49 +0700

    lint code

--- src/Backend/Tranchy.Question/Mappers/QuestionMapper.cs
+++ src/Backend/Tranchy.Question/Mappers/QuestionMapper.cs
@@ -1,17 +1,16 @@
 ﻿using Tranchy.Question.Contracts;
 
-namespace Tranchy.Question.Mappers
+namespace Tranchy.Question.Mappers;
+
+internal static class QuestionMapper
 {
-    internal static class QuestionMapper
+    internal static Data.Question ToDbQuestion(this CreateQuestionInput questionInput, string userId)
+    => new()
     {
-        internal static Data.Question ToDbQuestion(this CreateQuestionInput questionInput, string userId)
-        => new()
-        {
-            Status = Data.QuestionStatus.New,
-            SupportLevel = questionInput.SupportLevel,
-            Title = questionInput.Title,
-            QuestionCategoryIds = questionInput.QuestionCategoryIds,
-            CreatedByUserId = userId
-        };
-    }
+        Status = Data.QuestionStatus.New,
+        SupportLevel = questionInput.SupportLevel,
+        Title = questionInput.Title,
+        QuestionCategoryIds = questionInput.QuestionCategoryIds,
+        CreatedByUserId = userId
+    };
 }
