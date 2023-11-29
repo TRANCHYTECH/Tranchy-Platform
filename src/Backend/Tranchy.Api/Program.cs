1c490c09edf8170cea181d9f48e313c996525504
Tau Dang Ngoc <tau.dangngoc@medgate.ch>
2023-11-29 16:33:31 +0700

    Init new project

---
+++ src/Backend/Tranchy.Api/Program.cs
@@ -0,0 +1,57 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+string[] summaries =
+[
+    "Freezing",
+    "Bracing",
+    "Chilly",
+    "Cool",
+    "Mild",
+    "Warm",
+    "Balmy",
+    "Hot",
+    "Sweltering",
+    "Scorching"
+];
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+#pragma warning disable IDE0040 // Add accessibility modifiers
+#pragma warning disable MA0047 // Declare types in namespaces
+sealed record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+#pragma warning restore MA0047 // Declare types in namespaces
+#pragma warning restore IDE0040 // Add accessibility modifiers
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
