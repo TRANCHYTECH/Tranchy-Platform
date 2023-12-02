49dd10403262358e763d99c05df8dff4d747e3f4
TauDang <dangngoctau@gmail.com>
2023-12-02 23:13:02 +0700

    added theme sakai

--- src/Frontend/tranchy/apps/agencyportal/src/app/app.config.ts
+++ src/Frontend/tranchy/apps/agencyportal/src/app/app.config.ts
@@ -1,4 +1,9 @@
-import { HttpClient, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  HttpClient,
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
@@ -11,28 +16,33 @@ import { AskApiHttpInterceptor, CoreConfig, provideCore } from '@tranchy/core';
 import { environment } from '../environments/environment';
 import { appRoutes } from './app.routes';
 import { SharedModule } from '@tranchy/shared';
+import { AppLayoutModule } from './layout/app.layout.module';
 
 export interface PortalConfig extends CoreConfig {
-  production: boolean
+  production: boolean;
 }
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    provideHttpClient(withInterceptorsFromDi(), withInterceptors([AskApiHttpInterceptor])),
+    provideHttpClient(
+      withInterceptorsFromDi(),
+      withInterceptors([AskApiHttpInterceptor])
+    ),
     provideAnimations(),
     importProvidersFrom(
       TranslateModule.forRoot({
         loader: {
           provide: TranslateLoader,
           useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-        }
+          deps: [HttpClient],
+        },
       }),
+      AppLayoutModule,
       SharedModule.forRoot()
     ),
-    provideCore(environment)
-  ]
+    provideCore(environment),
+  ],
 };
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
